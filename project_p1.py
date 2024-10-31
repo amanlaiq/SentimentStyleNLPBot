cce87aa9702a797d3ebb5cf312cd69216a7708f9
amanlaiq <amoha211@uic.edu>
2024-10-31 12:11:17 -0500

    Added comments

--- project_p1.py
+++ project_p1.py
@@ -2,8 +2,11 @@ import pandas as pd
 import numpy as np
 import pickle as pkl
 import nltk
+nltk.download('vader_lexicon')
 import time
 import csv
+import re
+import string
 
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import GaussianNB
@@ -11,8 +14,10 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.svm import LinearSVC
 from sklearn.neural_network import MLPClassifier
 from sklearn.metrics import precision_score, recall_score, f1_score, accuracy_score
+from nltk.parse.corenlp import CoreNLPDependencyParser
+from nltk.tag import pos_tag
+from nltk.sentiment import SentimentIntensityAnalyzer
 
-nltk.download('punkt_tab')
 EMBEDDING_FILE = "w2v.pkl"
 
 def load_w2v(filepath):
@@ -25,6 +30,18 @@ def load_as_list(fname):
     labels = df['label'].values.tolist()
     return documents, labels
 
+def extract_user_info(user_input):
+    name = ""
+    name_match = re.search(r"(^|\s)([A-Z][A-Za-z-&'\.]*(\s|$)){2,4}", user_input)
+    if name_match is not None:
+        name = name_match.group(0).strip()
+    return name
+
+
+# Function to convert a given string into a list of tokens
+# Args:
+#   inp_str: input string 
+# Returns: token list, dtype: list of strings
 def get_tokens(inp_str):
     # Initialize NLTK tokenizer
     try:
@@ -34,16 +51,28 @@ def get_tokens(inp_str):
         nltk.download('punkt')
     return nltk.tokenize.word_tokenize(inp_str)
 
+
+# Function: vectorize_train, see project statement for more details
+# training_documents: A list of strings
+# Returns: An initialized TfidfVectorizer model, and a document-term matrix, dtype: scipy.sparse.csr.csr_matrix
 def vectorize_train(training_documents):
-    # Initialize the TfidfVectorizer model and document-term matrix
     vectorizer = None
     tfidf_train = None
-
     vectorizer = TfidfVectorizer(tokenizer=get_tokens, lowercase=True)
     tfidf_train = vectorizer.fit_transform(training_documents)
 
     return vectorizer, tfidf_train
 
+
+# Function: w2v(word2vec, token)
+# word2vec: The pretrained Word2Vec representations as dictionary
+# token: A string containing a single token
+# Returns: The Word2Vec embedding for that token, as a numpy array of size (300,)
+#
+# This function provides access to 300-dimensional Word2Vec representations
+# pretrained on Google News.  If the specified token does not exist in the
+# pretrained model, it should return a zero vector; otherwise, it returns the
+# corresponding word vector from the word2vec dictionary.
 def w2v(word2vec, token):
     word_vector = np.zeros(300,)
 
@@ -53,6 +82,14 @@ def w2v(word2vec, token):
     return word_vector
 
 
+# Function: string2vec(word2vec, user_input)
+# word2vec: The pretrained Word2Vec model
+# user_input: A string of arbitrary length
+# Returns: A 300-dimensional averaged Word2Vec embedding for that string
+#
+# This function preprocesses the input string, tokenizes it using get_tokens, extracts a word embedding for
+# each token in the string, and averages across those embeddings to produce a
+# single, averaged embedding for the entire input.
 def string2vec(word2vec, user_input):
     embedding = np.zeros(300,)
     tokens_list = []
@@ -68,22 +105,46 @@ def string2vec(word2vec, user_input):
     return embedding
 
 
+# Function: instantiate_models()
+# This function does not take any input
+# Returns: Four instantiated machine learning models
+#
+# This function instantiates the four imported machine learning models, and
+# returns them for later downstream use.  You do not need to train the models
+# in this function.
 def instantiate_models():
     nb = GaussianNB()
     mlp = MLPClassifier(random_state=100)
     logistic = LogisticRegression(random_state=100)
     svm = LinearSVC(random_state=100)
 
-
     return nb, logistic, svm, mlp
 
+
+# Function: train_model_tfidf(model, word2vec, training_documents, training_labels)
+# model: An instantiated machine learning model
+# tfidf_train: A document-term matrix built from the training data
+# training_labels: A list of integers (all 0 or 1)
+# Returns: A trained version of the input model
+#
+# This function trains an input machine learning model using TFIDF
+# embeddings for the training documents.
 def train_model_tfidf(model, tfidf_train, training_labels):
     tfidf_t = tfidf_train.toarray()
-
     model.fit(tfidf_t, training_labels)
 
     return model
 
+
+# Function: train_model_w2v(model, word2vec, training_documents, training_labels)
+# model: An instantiated machine learning model
+# word2vec: A pretrained Word2Vec model
+# training_data: A list of training documents
+# training_labels: A list of integers (all 0 or 1)
+# Returns: A trained version of the input model
+#
+# This function trains an input machine learning model using averaged Word2Vec
+# embeddings for the training documents.
 def train_model_w2v(model, word2vec, training_documents, training_labels):
     i = []
     for j in training_documents:
@@ -96,6 +157,17 @@ def train_model_w2v(model, word2vec, training_documents, training_labels):
     return model
 
 
+# Function: test_model_tfidf(model, word2vec, training_documents, training_labels)
+# model: An instantiated machine learning model
+# vectorizer: An initialized TfidfVectorizer model
+# test_data: A list of test documents
+# test_labels: A list of integers (all 0 or 1)
+# Returns: Precision, recall, F1, and accuracy values for the test data
+#
+# This function tests an input machine learning model by extracting features
+# for each preprocessed test document and then predicting an output label for
+# that document.  It compares the predicted and actual test labels and returns
+# precision, recall, f1, and accuracy scores.
 def test_model_tfidf(model, vectorizer, test_documents, test_labels):
     precision = None
     recall = None
@@ -113,6 +185,18 @@ def test_model_tfidf(model, vectorizer, test_documents, test_labels):
 
     return precision, recall, f1, accuracy
 
+
+# Function: test_model_w2v(model, word2vec, training_documents, training_labels)
+# model: An instantiated machine learning model
+# word2vec: A pretrained Word2Vec model
+# test_data: A list of test documents
+# test_labels: A list of integers (all 0 or 1)
+# Returns: Precision, recall, F1, and accuracy values for the test data
+#
+# This function tests an input machine learning model by extracting features
+# for each preprocessed test document and then predicting an output label for
+# that document.  It compares the predicted and actual test labels and returns
+# precision, recall, f1, and accuracy scores.
 def test_model_w2v(model, word2vec, test_documents, test_labels):
     precision = None
     recall = None
@@ -133,112 +217,3 @@ def test_model_w2v(model, word2vec, test_documents, test_labels):
 
     return precision, recall, f1, accuracy
 
-if __name__ == "__main__":
-    print("*************** Loading data & processing *****************")
-    print("Loading dataset.csv....")
-    documents, labels = load_as_list("dataset.csv")
-    
-    print("Loading Word2Vec representations....")
-    word2vec = load_w2v(EMBEDDING_FILE)
-
-    print("Computing TFIDF representations....")
-    vectorizer, tfidf_train = vectorize_train(documents)
-
-
-    print("\n**************** Training models ***********************")
-    print("Instantiating models....")
-    nb_tfidf, logistic_tfidf, svm_tfidf, mlp_tfidf = instantiate_models()
-    nb_w2v, logistic_w2v, svm_w2v, mlp_w2v = instantiate_models()
-
-    print("Training Naive Bayes models....")
-    start = time.time() 
-    nb_tfidf = train_model_tfidf(nb_tfidf, tfidf_train, labels)
-    end = time.time()
-    print("Naive Bayes + TFIDF trained in {0} seconds".format(end - start))
-
-    start = time.time()
-    nb_w2v = train_model_w2v(nb_w2v, word2vec, documents, labels)
-    end = time.time()
-    print("Naive Bayes + w2v trained in {0} seconds".format(end - start))
-
-    print("Training Logistic Regression models....")
-    start = time.time()
-    logistic_tfidf = train_model_tfidf(logistic_tfidf, tfidf_train, labels)
-    end = time.time()
-    print("Logistic Regression + TFIDF trained in {0} seconds".format(end - start))
-
-    start = time.time()
-    logistic_w2v = train_model_w2v(logistic_w2v, word2vec, documents, labels)
-    end = time.time()
-    print("Logistic Regression + w2v trained in {0} seconds".format(end - start))
-
-    print("Training SVM models....")
-    start = time.time()
-    svm_tfidf = train_model_tfidf(svm_tfidf, tfidf_train, labels)
-    end = time.time()
-    print("SVM + TFIDF trained in {0} seconds".format(end - start))
-
-    start = time.time()
-    svm_w2v = train_model_w2v(svm_w2v, word2vec, documents, labels)
-    end = time.time()
-    print("SVM + w2v trained in {0} seconds".format(end - start))
-
-    print("Training Multilayer Perceptron models....")
-    start = time.time()
-    mlp_tfidf = train_model_tfidf(mlp_tfidf, tfidf_train, labels)
-    end = time.time()
-    print("Multilayer Perceptron + TFIDF trained in {0} seconds".format(end - start))
-
-    start = time.time()
-    mlp_w2v = train_model_w2v(mlp_w2v, word2vec, documents, labels)
-    end = time.time()
-    print("Multilayer Perceptron + w2v trained in {0} seconds".format(end - start))
-
-    # print("Word2Vec embedding for {0}:\t{1}".format("vaccine", w2v(word2vec, "vaccine")))
-
-    print("\n***************** Testing models ***************************")
-    test_documents, test_labels = load_as_list("test.csv")  # Loading the dataset
-
-    models_tfidf = [nb_tfidf, logistic_tfidf, svm_tfidf, mlp_tfidf]
-    models_w2v = [nb_w2v, logistic_w2v, svm_w2v, mlp_w2v]
-    model_names = ["Naive Bayes", "Logistic Regression", "SVM", "Multilayer Perceptron"]
-
-    outfile = open("classification_report.csv", "w", newline='\n')
-    outfile_writer = csv.writer(outfile)
-    outfile_writer.writerow(["Name", "Precision", "Recall", "F1", "Accuracy"]) # Header row
-
-    i = 0
-    while i < len(models_tfidf): # Loop through models
-        print("Making predictions for " + model_names[i] + "....")
-        p, r, f, a = test_model_tfidf(models_tfidf[i], vectorizer, test_documents, test_labels)
-        if models_tfidf[i] is None:  # Models will be None if functions have not yet been implemented
-            outfile_writer.writerow([model_names[i] + " + TFIDF", "N/A", "N/A", "N/A", "N/A"])
-        else:
-            outfile_writer.writerow([model_names[i] + " + TFIDF", p, r, f, a])
-
-        p, r, f, a = test_model_w2v(models_w2v[i], word2vec, test_documents, test_labels)
-        if models_w2v[i] is None: # Models will be None if functions have not yet been implemented
-            outfile_writer.writerow([model_names[i]+" + w2v","N/A", "N/A", "N/A", "N/A"])
-        else:
-            outfile_writer.writerow([model_names[i]+" + w2v", p, r, f, a])
-        i += 1
-    outfile.close()
-
-
-    print("\n************** Beginning chatbot execution *******************")
-
-    user_input = input("Welcome to the CS 421 chatbot!  What do you want to talk about today?\n")
-
-    w2v_test = string2vec(word2vec, user_input) # This assumes you're using Word2Vec representations
-
-    label = None
-
-    if mlp_w2v is not None:
-        label = mlp_w2v.predict(w2v_test.reshape(1, -1)) # This assumes you're using mlp_w2v; feel free to update
-
-        if label == 0:
-            print("Hmm, it seems like you're feeling a bit down.")
-        elif label == 1:
-            print("It sounds like you're in a positive mood!")
-        else:
-            print("Hmm, that's weird.  My classifier predicted a value of: {0}".format(label))
